937e00d7f6c78426894dededa54ca7045deb3fcd
Nathan Bell <nathan.bell421@icloud.com>
2020-12-13 00:06:35 -0600

    add model users and view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,6 +18,17 @@ namespace viewModelFun.Controllers
             _logger = logger;
         }
 
+        [HttpGet("")]
+        public IActionResult Names()
+        {
+            // to a View that has defined a model as @model string[]
+            string[] names = new string[]
+            {
+                "Sally", "Billy", "Joey", "Moose"
+            };
+            return View(names);
+        }
+
         public IActionResult Index()
         {
             return View();
